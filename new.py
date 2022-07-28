799498dccc8988a592bd7fca0774b19dd6bc12c1
Владимир Иванов <ivs180397@yandex.ru>
2022-07-28 20:33:05 +0300

    конфиг

--- new.py
+++ new.py
@@ -1,4 +1,22 @@
-print('hi')
+from math import *
+import itertools
 
-x = 5
-print(x)
+message = 'Добро пожаловать в самую лучшую программу для вычисления ' \
+          'квадратного корня из заданного числа'
+
+
+def Cal_sqr_Root(tale):
+    """ Вычисляет квадратный корень"""
+
+    return sqrt(tale)
+
+
+def calc(your_number):
+    if your_number <= 0:
+        return
+
+    print(f"Мы вычислили корень квадратный из введенного вами числа. Это будет: {Cal_sqr_Root(your_number)}")
+
+
+print(message)
+calc(25.5)
